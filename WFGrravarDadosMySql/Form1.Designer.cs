dd257ace1c7eae6859dcada1eaedeec74765f552
MATEUS DE SOUZA LOPES <mateus.slopes8@senacsp.edu.br>
2023-07-19 17:14:24 -0300

    19/07 17:14

--- WFGrravarDadosMySql/Form1.Designer.cs
+++ WFGrravarDadosMySql/Form1.Designer.cs
@@ -36,12 +36,17 @@ namespace WFGrravarDadosMySql
             this.txtTelefone = new System.Windows.Forms.TextBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lstContato = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 73);
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(34, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 15);
             this.label1.TabIndex = 0;
@@ -51,7 +56,8 @@ namespace WFGrravarDadosMySql
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 171);
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(34, 118);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 15);
             this.label2.TabIndex = 1;
@@ -60,7 +66,8 @@ namespace WFGrravarDadosMySql
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 265);
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(34, 176);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 15);
             this.label3.TabIndex = 2;
@@ -68,7 +75,7 @@ namespace WFGrravarDadosMySql
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(182, 369);
+            this.button1.Location = new System.Drawing.Point(182, 241);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -78,30 +85,73 @@ namespace WFGrravarDadosMySql
             // 
             // txtTelefone
             // 
-            this.txtTelefone.Location = new System.Drawing.Point(34, 198);
+            this.txtTelefone.Location = new System.Drawing.Point(34, 136);
             this.txtTelefone.Name = "txtTelefone";
             this.txtTelefone.Size = new System.Drawing.Size(223, 23);
             this.txtTelefone.TabIndex = 4;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(34, 109);
+            this.txtNome.Location = new System.Drawing.Point(34, 80);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(223, 23);
             this.txtNome.TabIndex = 5;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(34, 297);
+            this.txtEmail.Location = new System.Drawing.Point(34, 194);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(223, 23);
             this.txtEmail.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Buscar Contato";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(335, 80);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(394, 23);
+            this.txtBuscar.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(739, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Buscar ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lstContato
+            // 
+            this.lstContato.HideSelection = false;
+            this.lstContato.Location = new System.Drawing.Point(335, 109);
+            this.lstContato.Name = "lstContato";
+            this.lstContato.Size = new System.Drawing.Size(394, 146);
+            this.lstContato.TabIndex = 10;
+            this.lstContato.UseCompatibleStateImageBehavior = false;
+            this.lstContato.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(299, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(810, 334);
+            this.Controls.Add(this.lstContato);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.txtTelefone);
@@ -109,6 +159,7 @@ namespace WFGrravarDadosMySql
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -125,6 +176,10 @@ namespace WFGrravarDadosMySql
         private System.Windows.Forms.TextBox txtTelefone;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView lstContato;
     }
 }
 
--- WFGrravarDadosMySql/Form1.cs
+++ WFGrravarDadosMySql/Form1.cs
@@ -13,7 +13,8 @@ namespace WFGrravarDadosMySql
 {
     public partial class Form1 : Form
     {
-        MySqlConnection Conexao;
+        private MySqlConnection Conexao;
+        private string data_source = "datasource=localhost;username=root;password=;database=db_agenda;";
         public Form1()
         {
             InitializeComponent();
@@ -28,7 +29,7 @@ namespace WFGrravarDadosMySql
         {
             try
             {
-                string data_source = "datasource=localhost;username=root;password=;database=db_agenda;";
+                
                 Conexao = new MySqlConnection(data_source);
 
                 string sql = "INSERT INTO contato (nome,email,telefone)"+
@@ -54,6 +55,59 @@ namespace WFGrravarDadosMySql
 
 
             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexao = new MySqlConnection(data_source);
+
+                string q = " '%" + txtBuscar.Text + "%' ";
+
+
+                string sql = "SELECT * FROM contato WHERE nome LIKE"+q+"OR email LIKE"+q;
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                lstContato.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                    reader.GetString(1),
+                    reader.GetString(2),
+                    reader.GetString(3),
+                    };
+
+                    var linhaListView = new ListViewItem(row);
+                    lstContato.Items.Add(linhaListView);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
         }
     }
 }
